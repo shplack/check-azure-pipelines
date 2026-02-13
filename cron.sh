d184cc6c07d77ec2fd8a541f27b7497418c81555
Alexander Hedberg <alexander.hedberg@epiroc.com>
2026-02-13 14:51:05 +0100

    Add repository update check in cron.sh before executing pipeline checks

--- cron.sh
+++ cron.sh
@@ -3,6 +3,16 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR" || exit 1
 
+# check if repo is updated and if updated, pull changes and exit
+git fetch origin main
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse @{u})
+if [ "$LOCAL" != "$REMOTE" ]; then
+  echo "Repo is not up to date. Pulling changes..."
+  git pull origin main
+  exit 0
+fi
+
 if ! ./check-azure-pipelines.sh -q -i 953; then
   ./show-color.sh red
   exit 1
